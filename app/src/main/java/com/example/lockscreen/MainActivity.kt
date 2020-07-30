5b8c8d693e1d1523a5d4c1d6f889afa2cc37370d
Zigfruid <Zigfruid@gmail.com>
2020-07-30 18:13:22 +0500

    design was done

---
+++ app/src/main/java/com/example/lockscreen/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lockscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        btnActivator.setOnClickListener {
+            if (etPassword.text.isNotEmpty()){
+                Toast.makeText(this, "Вы успешно активировали программу", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LockScreenActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "Заполните поле !!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}

2acbec5ec12ce6a76c4d5228b5ae02a8f421a469
Zigfruid <Zigfruid@gmail.com>
2020-07-30 23:56:30 +0500

    was not done also

--- app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
+++ app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
@@ -1,12 +1,41 @@
 package com.example.lockscreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActivityManager
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_lock_screen.*
 
 class LockScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock_screen)
         supportActionBar?.hide()
+
+
+        btnExit.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    override fun onBackPressed() {
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+         if(keyCode == KeyEvent.KEYCODE_HOME){
+            Toast.makeText(this, "вы нажали на кнопку назад", Toast.LENGTH_SHORT).show()
+        }
+        return super.onKeyDown(0, event)
+    }
+
+    override fun onPause(): Unit {
+        super.onPause()
+        val activityManager = applicationContext
+            .getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.moveTaskToFront(taskId, 0)
     }
 }

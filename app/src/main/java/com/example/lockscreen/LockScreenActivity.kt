9a8ce8e287daa743e1e9486dbc6eb615575d2b03
Nalog00 <bisenovsaliq@gmail.com>
2020-07-31 13:37:01 +0500

    addConCallSms

--- app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
+++ app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
@@ -1,12 +1,33 @@
 package com.example.lockscreen
 
+import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_DIAL
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_lock_screen.*
+import java.util.jar.Manifest
 
 class LockScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock_screen)
         supportActionBar?.hide()
+        btnCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            startActivity(intent)
+        }
+        btnSms.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+            startActivity(intent)
+        }
+        btnContacts.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_CONTACTS)
+            startActivity(intent)
+        }
     }
 }

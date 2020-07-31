0a5963d280b08776914405cf63aa1b7215bc6906
Zigfruid <Zigfruid@gmail.com>
2020-07-31 23:07:06 +0500

    was not done

--- app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
+++ app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
@@ -4,7 +4,6 @@ import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import android.view.View
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_lock_screen.*
@@ -15,12 +14,25 @@ class LockScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock_screen)
         supportActionBar?.hide()
-        val actionBar = actionBar
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
-//        val root = Runtime.getRuntime().exec("su")
-//        root.waitFor()
+
+        btnCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            startActivity(intent)
+        }
+
+        btnSms.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+            startActivity(intent)
+        }
+        btnContacts.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_CONTACTS)
+            startActivity(intent)
+        }
 
             btnExit.setOnClickListener {
                 onDestroy()
@@ -33,21 +45,8 @@ class LockScreenActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
 
-        btnCall.setOnClickListener {
-            val intent = Intent(Intent.ACTION_DIAL)
-            startActivity(intent)
-        }
-        btnSms.setOnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
-            startActivity(intent)
-        }
-        btnContacts.setOnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.addCategory(Intent.CATEGORY_APP_CONTACTS)
-            startActivity(intent)
-        }
     }
+
 
     override fun onPause(): Unit {
         super.onPause()

1cef83689a0fae9d55442d7a5fcc52117208ec06
Zigfruid <Zigfruid@gmail.com>
2020-07-31 22:36:49 +0500

    was not done

--- app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
+++ app/src/main/java/com/example/lockscreen/LockScreenActivity.kt
@@ -2,22 +2,35 @@ package com.example.lockscreen
 
 import android.app.ActivityManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
-import android.widget.Toast
+import android.view.KeyEvent.*
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_lock_screen.*
 
+
 class LockScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock_screen)
         supportActionBar?.hide()
+        val actionBar = actionBar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+//        val root = Runtime.getRuntime().exec("su")
+//        root.waitFor()
+
+            btnExit.setOnClickListener {
+                onDestroy()
+                finish()
 
 
-        btnExit.setOnClickListener {
-            finish()
-        }
+            }
 
     }
 
@@ -25,17 +38,12 @@ class LockScreenActivity : AppCompatActivity() {
 
     }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-         if(keyCode == KeyEvent.KEYCODE_HOME){
-            Toast.makeText(this, "вы нажали на кнопку назад", Toast.LENGTH_SHORT).show()
-        }
-        return super.onKeyDown(0, event)
-    }
-
     override fun onPause(): Unit {
         super.onPause()
         val activityManager = applicationContext
             .getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         activityManager.moveTaskToFront(taskId, 0)
+
     }
+
 }
